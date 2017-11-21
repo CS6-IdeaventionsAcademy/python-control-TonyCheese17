31a550fdde13b1f7e38f561aeea18ad5725af8d9
ia-acollins <acollins@ideaventionsacademy.org>
2017-11-21 11:14:06 -0500

    Missile Launch
    My password checker thing for the Python Problem Set 1.2

--- c1_6_password_checker.py
+++ c1_6_password_checker.py
@@ -1,5 +1,55 @@
-# Name
-# Date
+# Tony Yu
+# 11/15/17
 # Python Beginnings
 # 6 Password Checker
+import time
 
+print('booting',end='')
+time.sleep(0.3)
+print(".",end='')
+time.sleep(0.3)
+print(".",end='')
+time.sleep(0.3)
+print(".",end='')
+time.sleep(0.3)
+print(".",end='')
+
+print()
+
+time.sleep(0.8)
+
+print("all systems operational")
+
+print()
+
+time.sleep(0.6)
+
+print("Welcome to missile launch pad 21AT")
+
+time.sleep(0.4)
+
+print()
+
+attempt = input("Please enter password to countinue: ")
+
+print()
+
+if attempt == ("Cheesetony11"):
+    time.sleep(0.4)
+    print("correct")
+    print()
+    print("launching missile in")
+    time.sleep(1)
+    print("3.")
+    time.sleep(1)
+    print("2.")
+    time.sleep(1)
+    print("1.")
+    print("Missile 21AT has been launched")
+else:
+    print("access denied")
+    print()
+    print()
+    print()
+    print("lel")
+          
