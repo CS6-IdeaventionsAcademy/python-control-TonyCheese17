2ad2f99055a7443a10a6e733ad6fc5b6d96aa194
TonyCheese11 <wyu@ideaventionsacadey.org>
2017-11-21 11:41:33 -0500

    color guesser
    my color guesser program for Python Problem set 1.2

--- c1_7_favorite_color.py
+++ c1_7_favorite_color.py
@@ -1,5 +1,43 @@
-# Name
-# Date
+# Tony Yu
+# 11/21/17
 # Python Beginnings
 # 7 Favorite Color
+import time
 
+color = ("green")
+
+print("$" * 50)
+print("Welcome to Tony's color guesser!!!")
+print("$" * 50)
+
+print()
+
+time.sleep(0.5)
+
+print("You will have to guess my favorite color!")
+
+print()
+
+time.sleep(0.5)
+
+print("If you get it right, you get to get a message saying you did!")
+
+print()
+
+time.sleep(0.5)
+
+print("If you don't, you get a message saying you didn't! Good luck!!!")
+
+print()
+
+time.sleep(0.5)
+
+guess = input("What is your guess: ")
+
+guess = guess.lower()
+
+while guess != color:
+    print("You got it wrong.")
+    guess = input("What is your guess: ")
+    guess = guess.lower()
+print("Good job! You got it right.")
